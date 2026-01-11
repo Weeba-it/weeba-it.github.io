3973e1cdf221683d2cebcce41b3d773b6495f034
classe 4id <giovanni.signorile@marconibari.edu.it>
2026-01-11 16:59:20 +0100

    upldate dist files after removing old html files and adding new assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ← QUESTO EVITA IL SITO BIANCO
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // ora @ punta a src
+    },
+  },
 });
