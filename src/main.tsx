0ea8a5695d298cba36add0de98cc82b0f515d988
classe 4id <giovanni.signorile@marconibari.edu.it>
2026-01-11 18:05:10 +0100

    404

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Handle SPA routing for GitHub Pages
-const handleGitHubPagesRouting = () => {
-  if (window.location.pathname.startsWith('/?/')) {
-    const path = window.location.pathname.slice(2); // Remove '/?'
-    window.history.replaceState(null, '', path + window.location.search);
-  }
-};
-
-handleGitHubPagesRouting();
-
 createRoot(document.getElementById("root")!).render(<App />);
