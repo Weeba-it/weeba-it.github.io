14e021d4e63a22c62195a6d9a3c11c37244e9ab1
Weeba-it <weebastaff@gmail.com>
2026-01-12 08:33:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Weeba-it.github.io/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // ora @ punta a src
